f0ba68c181d6c03bf10e4895f0fe14e365fb967f
haitham911 <haitham911eg@gmail.com>
2025-03-12 02:37:41 +0200

    update

--- main.go
+++ main.go
@@ -4,26 +4,22 @@ import (
 	"fmt"
 )
 
-// Incorrect naming convention (should be camelCase)
-func Print_Message() {
+// Correct naming convention (camelCase)
+func PrintMessage() {
 	fmt.Println("Hello, GolangCI-Lint!")
+	fmt.Println("Hello, GolangCI-Lint!")
+
 }
 
-// Unnecessary else block
-func checkNumber(n int) string {
+// Simplified without unnecessary else block
+func checkNumber(n int, t int) string {
 	if n > 0 {
 		return "Positive"
-	} else { // `revive` will flag this as an unnecessary else block
-		return "Non-positive"
 	}
-}
-
-// Unused function
-func unusedFunction() { // `revive` will flag this as an unused function
-	fmt.Println("This function is not used")
+	return "Non-positive"
 }
 
 func main() {
-	Print_Message()
-	fmt.Println(checkNumber(-5))
+	PrintMessage()
+	fmt.Println(checkNumber(-5, 0))
 }
